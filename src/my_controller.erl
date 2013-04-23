63367c244cad0264626c9a97baa60660fa6b8627
Stu Bailey <sbailey@infoblox.com>
2013-04-23 15:45:48 -0700

    Working ONS demo

--- src/my_controller.erl
+++ src/my_controller.erl
@@ -1,17 +1,30 @@
 -module(my_controller).
 
--export([start/0,link/2,tap/2]).
+-export([start/0,link/2,tap/3,gate/2,clear/0]).
 
 start()->
     loom_controller:start().
+ %   timer:sleep(2000),
+
+clear()->
+    loom_controller:clear_all_flow_mods().
+
 
 
 link(Port1,Port2)->
-    Mod1 = loom_flow_lib:gate_mod(Port1,Port2),
+    Mod1 = loom_flow_lib:gate_mod(Port1,Port2,12),
     loom_controller:broadcast_flow_mod(Mod1),
-    Mod2 = loom_flow_lib:gate_mod(Port2,Port1),
+    Mod2 = loom_flow_lib:gate_mod(Port2,Port1,12),
     loom_controller:broadcast_flow_mod(Mod2).
 
-tap(InPort,OutPorts)->
-    Mod1 = loom_flow_lib:tap_mod(InPort,OutPorts),
+tap(InPort,OutPort,TapPort)->
+    gate(InPort,OutPort),
+    gate(OutPort,TapPort),
+    gate(TapPort,InPort).
+%    Mod1 = loom_flow_lib:tap_mod(InPort,OutPorts,Seq),
+%    loom_controller:broadcast_flow_mod(Mod1).
+
+
+gate(InPort,OutPort)->
+    Mod1= loom_flow_lib:gate_mod(InPort,OutPort,14),
     loom_controller:broadcast_flow_mod(Mod1).
--- src/my_executive.erl
+++ src/my_executive.erl
@@ -1,6 +1,12 @@
 -module(my_executive).
 
--export([start/0,add_capable_switch/1,get_config/1,get_config_xml/1,print_config_xml/1]).
+-export([start/0,add_capable_switch/1,get_config/1,get_config_xml/1,print_config_xml/1,
+	tilera_linc/0,linc3/0,linc4/0,pi_linc/0]).
+
+-define(TILERA,{"10.192.168.152",[{port, 1831}, {user, "linc"}, {password, "linc"}]}).
+-define(LINC3,{"10.192.168.152",[{port, 1830}, {user, "linc"}, {password, "linc"}]}).
+-define(LINC4,{"10.192.168.158",[{port, 1830}, {user, "linc"}, {password, "linc"}]}).
+-define(PI,{"10.192.168.163",[{port, 1830}, {user, "linc"}, {password, "linc"}]}).
 
 
 capable_switch(IP)->
@@ -14,20 +20,33 @@ add_capable_switch(IP)->
     CapableSwitch = capable_switch(IP),
     loom_executive:connect(CapableSwitch).
 
+tilera_linc()->
+    ?TILERA.
 
-get_config(IP)->
-    Config = get_config_xml(IP),
+linc3()->
+    ?LINC3.
+
+linc4()->
+    ?LINC4.
+
+pi_linc()->
+    ?PI.
+
+get_config(CapableSwitch)->
+    Config = get_config_xml(CapableSwitch),
     Result = enetconf_parser:convert(Config),
     {'rpc-reply',
      [{'message-id',_},{xmlns,_}],
      [{data,[],[ErlConfig]}]} = Result,
-    ErlConfig.
+    ErlConfig,
+    io:format("Config = ~p~n",[ErlConfig]).
 
-get_config_xml(IP)->
-    {ok,Connection} = enetconf_client:connect(IP,[{port, 1830}, {user, "linc"}, {password, "linc"}]),
+get_config_xml(CapableSwitch)->
+    {IP,Creds}=CapableSwitch,
+    {ok,Connection} = enetconf_client:connect(IP,Creds),
     {ok, Config} = enetconf_client:get_config(Connection, running),
     Config.
 
-print_config_xml(IP)->
-    Config = get_config_xml(IP),
+print_config_xml(CapableSwitch)->
+    Config = get_config_xml(CapableSwitch),
     io:format("Config = ~p~n",[Config]).
