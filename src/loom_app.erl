b91893876d9c9eb27555d7e698ab6237828daeb4
Stu Bailey <sbailey@infoblox.com>
2013-06-11 00:19:05 -0700

    fixed paths

--- src/loom_app.erl
+++ src/loom_app.erl
@@ -3,6 +3,8 @@
 -export[start/0].
 
 start()->
+    [code:add_pathz(Path) || Path <- filelib:wildcard("./deps/*/ebin")],
+    [code:add_pathz(Path) || Path <- filelib:wildcard("./apps/*/ebin")],
     error_logger:tty(false),
     ok = application:start(crypto),
     ok = application:start(public_key),
