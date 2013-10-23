02bbb3ec77399c44ca8cd106e11d4bdfb524028f
Stu Bailey <sbailey@infoblox.com>
2013-10-22 17:29:23 -0700

    Added DNS packet parsing

--- src/loom_ofdp_recv.erl
+++ src/loom_ofdp_recv.erl
@@ -222,9 +222,10 @@ process_message(Message, MessageCache, Socket) ->
 % Max len of list is 100
 process_packetin(action, _TableId, _Match, Data, #cache{packetin = {EthSrcList, EthDstList}} = MessageCache) ->
     try
-        [EthHeader | _] = pkt:decapsulate(Data),
+        {EthHeader , EthPayload } = pkt:decapsulate({ether,Data}),
         EthSrc = EthHeader#ether.shost,
         EthDst = EthHeader#ether.dhost,
+	EthType = EthHeader#ether.type,
         case lists:keyfind(EthSrc, 1, EthSrcList) of 
             false ->
                 NEthSrcList = lists:sublist([{EthSrc, 1} | EthSrcList], 100);
@@ -238,8 +239,25 @@ process_packetin(action, _TableId, _Match, Data, #cache{packetin = {EthSrcList, 
                 NEthDstList = lists:keyreplace(EthDst, 1, EthDstList, {EthDst, DCount+1})
         end,
         lager:info("packetin reason = action, EthSrc = ~18s, EthDSt = ~18s ~n",
-            [binary_to_hex(EthSrc), binary_to_hex(EthDst)]),
-        MessageCache#cache{packetin = {NEthSrcList, NEthDstList}}      
+		   [binary_to_hex(EthSrc), binary_to_hex(EthDst)]),
+	lager:info("Ether Packet of type",[EthType]),
+	{Header1,Payload1} = pkt:decapsulte({EthType,EthPayload}),
+	ProtoType = case EthType of
+		    ipv4 ->
+			Header1#ipv4.p;
+		    _ -> unknown
+		end,
+	lager:info("Network Packet of type",[ProtoType]),
+	{Header2,Payload2} = pkt:decapsulte({ProtoType,Payload1}),
+	Result = case ProtoType of
+		     udp -> inet_dns:decode(Payload2);
+		     _ -> unknown
+		 end,
+	case Result of
+	    {ok,DnsRec} -> lager:info("DNS Packet from ~p to ~p: ~p~n",[Header1#ipv4.saddr,Header1#ipv4.daddr,DnsRec]);
+	    _ -> ok
+	end,
+	MessageCache#cache{packetin = {NEthSrcList, NEthDstList}}      
     catch
         E1:E2 ->
 %            lager:error("Pkt decapsulate error: ~p:~p", [E1, E2]),
