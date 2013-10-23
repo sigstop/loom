63aca716b0a27c19b01a2455205c85280f99d1fe
Stu Bailey <sbailey@infoblox.com>
2013-10-23 13:44:22 -0700

    dpi final

--- src/loom_dpi_lib.erl
+++ src/loom_dpi_lib.erl
@@ -13,35 +13,36 @@
 -compile([export_all]).
 
 dns_reply(Pids,Data)->
-    lager:info("Inspecting packet: ~p~n",[Data]),
+%    lager:info("Inspecting packet: ~p~n",[Data]),
     try
 	Packet = pkt:decapsulate({ether,Data}),
 	case Packet of 
 	    [EthHeader,Header1,Header2,Payload] ->
-		lager:info("Header1: ~p~n",[Header1]),
-		lager:info("Header2: ~p~n",[Header2]),
+%		lager:info("Header1: ~p~n",[Header1]),
+%		lager:info("Header2: ~p~n",[Header2]),
 		[ether|_] = tuple_to_list(EthHeader),
 		[Type1|_] = tuple_to_list(Header1),
 		[Type2|_] = tuple_to_list(Header2),
-		lager:info("Network Packet of type ~p/~p~n",[Type1,Type2]),
+%		lager:info("Network Packet of type ~p/~p~n",[Type1,Type2]),
 		Result = case (Type1 == ipv4) and (Type2 == udp) of
 			     true ->  
-				      lager:info("Attempting inet_dns:decode(..) on ~p~n",[Payload]),
-				      inet_dns:decode(Payload);
+%				      lager:info("Attempting inet_dns:decode(..) on ~p~n",[Payload]),
+				 inet_dns:decode(Payload);
 			     _ -> unknown
 			 end,
 		case Result of
-		    {ok,DnsRec} -> lager:info("DNS Packet: ~p~n",[DnsRec]),
-				   Match = match_reply(DnsRec),
-				   lager:info("Match Value: ~p~n",[Match]),
-				   Return = case Match of
-						{error,_} -> error;
-						ID ->
-                                                    R = list_to_tuple(binary_to_list(Header1#ipv4.daddr)),
-                                                    {R,ID}
-					    end,
-				   lager:info("Return Value: ~p~n",[Return]),
-				   [ Pid ! {dns_reply,DnsRec} || Pid <- Pids ];
+		    {ok,DnsRec} -> 
+%			lager:info("DNS Packet: ~p~n",[DnsRec]),
+			Match = match_reply(DnsRec),
+%			lager:info("Match Value: ~p~n",[Match]),
+			Return = case Match of
+				     {error,_} -> error;
+				     ID ->
+					 R = list_to_tuple(binary_to_list(Header1#ipv4.daddr)),
+					 {R,ID}
+				 end,
+			lager:info("Return Value: ~p~n",[Return]),
+			[ Pid ! {dns_reply,DnsRec} || Pid <- Pids ];
 		    
 		    _ -> lager:info("No match dropped: ~p~n",[Result])
 		end;
