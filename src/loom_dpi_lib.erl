b5e42b0eb2eef79af244c14f9347ce07d40088c6
Stu Bailey <sbailey@infoblox.com>
2013-10-23 08:58:09 -0700

    Bug in dpi

--- src/loom_dpi_lib.erl
+++ src/loom_dpi_lib.erl
@@ -14,30 +14,33 @@
 
 dns_reply(Pids,Data)->
     try
-	{EthHeader , EthPayload } = pkt:decapsulate({ether,Data}),
-        EthSrc = EthHeader#ether.shost,
-        EthDst = EthHeader#ether.dhost,
-	EthType = EthHeader#ether.type,
-	lager:info("Ether Packet: EthSrc = ~18s, EthDSt = ~18s ~n",
-		   [loom_util:binary_to_hex(EthSrc), loom_util:binary_to_hex(EthDst)]),
-	lager:info("Ether Packet of type",[EthType]),
-	{Header1,Payload1} = pkt:decapsulte({EthType,EthPayload}),
-	ProtoType = case EthType of
-		    ipv4 ->
-			    Header1#ipv4.p;
-			_ -> unknown
-		    end,
-	lager:info("Network Packet of type",[ProtoType]),
-	{Header2,Payload2} = pkt:decapsulte({ProtoType,Payload1}),
-	Result = case ProtoType of
-		     udp -> inet_dns:decode(Payload2);
-		     _ -> unknown
-		 end,
-	case Result of
-	    {ok,DnsRec} -> lager:info("DNS Packet from ~p to ~p: ~p~n",[Header1#ipv4.saddr,Header1#ipv4.daddr,DnsRec]),
-			   [ Pid ! {dns_reply,DnsRec} || Pid <- Pids ];
-	    
-	    _ -> ok
+	Packet = pkt:decapsulate({ether,Data}),
+	case Packet of 
+	    [EthHeader,Header1,Header2,Payload] ->
+		EthSrc = EthHeader#ether.shost,
+		EthDst = EthHeader#ether.dhost,
+		EthType = EthHeader#ether.type,
+		lager:info("Ether Packet: EthSrc = ~18s, EthDSt = ~18s ~n",
+			   [loom_util:binary_to_hex(EthSrc), loom_util:binary_to_hex(EthDst)]),
+		lager:info("Ether Packet of type",[EthType]),
+		ProtoType = case EthType of
+				ipv4 ->
+				    Header1#ipv4.p;
+				_ -> unknown
+			    end,
+		lager:info("Network Packet of type",[ProtoType]),
+		Result = case ProtoType of
+			     udp -> inet_dns:decode(Payload);
+			     _ -> unknown
+			 end,
+		case Result of
+		    {ok,DnsRec} -> lager:info("DNS Packet from ~p to ~p: ~p~n",[Header1#ipv4.saddr,Header1#ipv4.daddr,DnsRec]),
+				   [ Pid ! {dns_reply,DnsRec} || Pid <- Pids ];
+		    
+		    _ -> ok
+		end;
+	    _ -> 
+		lager:info("No match dropped: ~p~n",[Packet])
 	end
     catch
 	Error ->
