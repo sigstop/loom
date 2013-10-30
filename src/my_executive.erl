9aae5dacbf29d1515dfa98e485d8f7d32744ba00
shivarammysore <shivaram.mysore@gmail.com>
2013-10-29 22:59:54 -0700

    Updated Copyright and license information

--- src/my_executive.erl
+++ src/my_executive.erl
@@ -1,3 +1,23 @@
+%%------------------------------------------------------------------------------
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%%-----------------------------------------------------------------------------
+%%
+%% @author Infoblox Inc <info@infoblox.com>
+%% @copyright 2013 Infoblox Inc
+%% @doc 
+
 -module(my_executive).
 
 -export([start/0,add_capable_switch/1,get_config/1,get_config_xml/1,print_config_xml/1,
